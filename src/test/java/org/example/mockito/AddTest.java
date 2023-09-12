da8736891bc06929c08adc2684ec08d93516c8c1
enriquenavarrocastro <kike1580@hotmail.com>
2023-09-12 21:41:07 +0200

    seguimos con mas de Mockito

--- src/test/java/org/example/mockito/AddTest.java
+++ src/test/java/org/example/mockito/AddTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.when;
 
 class AddTest {
@@ -25,6 +26,27 @@ class AddTest {
         when(validNumber.check(3)).thenReturn(false);
         boolean checkNumber = validNumber.check(3);
         assertEquals(false, checkNumber);
+    }
+
+    @Test
+    void addMockException(){
+        when(validNumber.checkZero(0)).thenThrow(new ArithmeticException("No podemos aceptar 0 "));
+        Exception exception = null;
+        try {
+            validNumber.checkZero(0);
+        }catch (ArithmeticException e){
+            exception = e;
+        }
+        assertNotNull(exception);
+    }
+
+    @Test
+    void addRealMethodTest(){
+        when(validNumber.check(3)).thenCallRealMethod();
+        assertEquals(true, validNumber.check(3));
+
+        when(validNumber.check(3)).thenCallRealMethod();
+        assertEquals(false, validNumber.check("3"));
     }
 
 
