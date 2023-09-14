1200ac3b58641466a92666be8bde30e1c70dc6f8
enavarrc <enrique.navarrocastro@nttdata.com>
2023-09-14 21:17:21 +0200

    Utilizando doAnswer para traer los valores reales para testearlos

--- src/main/java/org/example/mockito/Login.java
+++ src/main/java/org/example/mockito/Login.java
@@ -14,12 +14,14 @@ public class Login {
         webService.login("Alberto", "12345", new Callback() {
             @Override
             public void onSuccess(String response) {
-
+                System.out.println(response);
+                isLogin = true;
             }
 
             @Override
             public void onFail(String error) {
-
+                System.out.println(error);
+                isLogin = false;
             }
         });
     }
